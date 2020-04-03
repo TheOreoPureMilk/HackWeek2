1067c8b00abd1e334c36c48f204f28629029b98e
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-04-03 21:00:07 +0800

    登录页面布局

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import LoginForm from './components/LoginForm'
 import './Login.css'
+import logo from './logo.png'
+import { Link } from 'react-router-dom';
 
 function Login(props) {
   return (
     <div className="login-body">
-      <LoginForm />
+      <div className="register">
+        <Link to="#" style={{ float: "right", color: "white" }}>注册</Link>
+      </div>
+      <div className="logo-box">
+        <img className="login-logo" src={logo} alt="logo" />
+        <div className="head-text">MEET YOUR ANOTHER SELF</div>
+      </div>
+      <div className="login-form">
+        <LoginForm />
+      </div>
     </div>
   )
 }
--- src/pages/login/components/LoginForm.jsx
+++ src/pages/login/components/LoginForm.jsx
@@ -1,24 +1,46 @@
 import React from 'react'
 import { Input } from 'antd'
+import { Button } from 'antd';
 import { MailOutlined, UnlockOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom';
 
 const myForms = {
   width: "3.20rem",
-  height: "1.26rem"
+  height: "1.26rem",
+  borderRadius: "0.07rem"
 }
 
 const loginInputRadiusTop = {
-  height: "0.62rem",
-  fontSize: '0.3rem',
-  borderRadius: "0.07rem 0.07rem 0 0",
-  color: "#C0C0C0"
+  height: "60px",
+  fontSize: '30px',
+  borderRadius: "7px 7px 0 0",
+  color: "#C0C0C0",
+  boxShadow: "3px 3px 10px #646464"
 }
 
 const loginInputRadiusBon = {
-  height: "0.62rem",
-  fontSize: '0.3rem',
-  borderRadius: "0 0 0.07rem 0.07rem",
-  color: "#C0C0C0"
+  height: "60px",
+  fontSize: '30px',
+  borderRadius: "0 0 7px 7px",
+  color: "#C0C0C0",
+  boxShadow: "3px 3px 10px #646464"
+}
+
+const Submit = {
+  width: "136px",
+  height: "42px",
+}
+
+const SubmitTool = {
+  display: "flex",
+  justifyContent: "center",
+  marginTop: "30px",
+}
+
+const Unknown = {
+  display: "flex",
+  justifyContent: "center",
+  marginTop: "20px",
 }
 
 function LoginForm() {
@@ -27,15 +49,23 @@ function LoginForm() {
       <Input size="large"
         placeholder="邮箱"
         style={loginInputRadiusTop}
-        prefix={
-          <MailOutlined />}
+        prefix={<MailOutlined />}
       />
-      <Input size="large"
+      <Input.Password size="large"
         placeholder="密码"
         style={loginInputRadiusBon}
         prefix={<UnlockOutlined />}
       />
+      <div style={SubmitTool}>
+        <div style={Submit}>
+          <Button size="large" shape="round" ghost block>登录</Button>
+        </div>
+      </div>
+      <div style={Unknown}>
+        <Link to="#" style={{ color: "white" }}>忘记密码</Link>
+      </div>
     </div>
+
   )
 }
 
