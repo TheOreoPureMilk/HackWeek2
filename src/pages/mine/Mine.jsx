4e0d1bb044b80a2b6af01b60728c0070ea4b6a46
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-04-06 17:53:23 +0800

    基础我的页面布局

--- src/pages/mine/Mine.jsx
+++ src/pages/mine/Mine.jsx
@@ -3,18 +3,44 @@ import './Mine.css'
 
 
 function Mine(props) {
+  const messageCount = 1
   return (
     <div className='mine'>
       <div className='mine-head'>
-        <div className="mine-mes">
-          <img src="#" alt="钟"/>
+        <div className="mine-mes-line">
+          <div className='mes-box'>
+            <span >{messageCount}</span>
+          </div>
         </div>
-        <div className="head-text"></div>
-        <div className="user-info"></div>
+        <div className="mine-head-text">
+          我的
+        </div>
+        <div className="user-info">
+
+          <img alt='?????>>>>>' src={'#'} className='user-image' />
+          <div className='user-info-show'>
+            <span className='user-nick'>爱吃草的羊</span>
+            <span className='user-sex'>♂</span>
+          </div>
+          <button className='change-bg-btn'>
+            <span>
+              更换背景
+            </span>
+          </button>
+        </div>
+      </div >
+      <div className='mine-body'>
+        <div className='mine-body-tab'>
+          <div className='luck-item'>
+            <div className='luck-item-text'>幸运信</div>
+          </div>
+          <div className='sad-item'>
+            <div className='sad-item-text'>解忧信</div>
+          </div>
+        </div>
       </div>
-      <div className='mine-body'></div>
       <div className='footer'></div>
-    </div>
+    </div >
   )
 }
 
