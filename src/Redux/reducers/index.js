032810f903634e5cddc8942334bf16952c7cfd0c
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-04-07 21:18:43 +0800

    重写redux

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -1,13 +1,8 @@
 import { combineReducers } from 'redux'
-import loginLoad from './loading'
-import registerLoad from './registering'
-import Changeloadmes from './loadmes'
 
 const windApp = combineReducers(
   {
-    loginLoad,
-    registerLoad,
-    Changeloadmes
+
   }
 )
 
