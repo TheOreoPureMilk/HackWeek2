7bc087c317a7531ec7edd90cecb3e9a8f96aaf40
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-04-08 21:05:44 +0800

    定制一周列表组件样式

---
+++ src/pages/tree/components/ImgList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ImgList(props) {
+  return (
+    <div className='img-list'>
+      <div className='week-img'></div>
+    </div>
+  )
+}
+
+export default ImgList
