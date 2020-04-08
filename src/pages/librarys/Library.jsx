27634d8600e9da4fdc28e1fbf4ff786a346ecb02
TheOreoPureMilk <6109119146@email.nuc.edu.cn>
2020-04-08 14:35:52 +0800

    略微完善故事馆页面

--- src/pages/librarys/Library.jsx
+++ src/pages/librarys/Library.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Library.css'
 import Footer from '../../components/Footer/Footer'
+import editor_pen from './editor_pen.png'
+import test_head_img from './test_head_img.png'
+import next from './next.png'
+
 
 function Library(props) {
   return (
@@ -14,7 +18,20 @@ function Library(props) {
         </div>
       </div>
       <div className='letter-content'>
-
+        <div className='letter-content-head'>
+          <div className='user-head-img-content'>
+            <img src={test_head_img} alt='头像' className='user-head-img' />
+          </div>
+          <div className='user-name-content'>
+            <div className='lib-user-name'>爱吃草的羊</div>
+          </div>
+          <div className='next-button'>
+            <img src={next} alt='下一个' className="next-img" />
+          </div>
+        </div>
+      </div>
+      <div className='editor-pen-content'>
+        <img src={editor_pen} alt='pen' className='editor-pen-img' />
       </div>
       <div className='footer-body'>
         <Footer />
